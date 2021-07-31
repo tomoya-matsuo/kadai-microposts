91ef3b6162d67e31c6cc5ffdf1e498ff191f84b7
tomoya-matsuo <musiclove_drummer@ezweb.ne.jp>
2021-07-31 06:41:04 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ユーザー登録
+Route::get('signup','Auth\RegisterController@showRegistration
+Form')->name('signup.get');
+Route::post('signup','Auth\RegisterController@register')
+->name('signup.post');
